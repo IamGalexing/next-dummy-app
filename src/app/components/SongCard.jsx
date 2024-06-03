d876f95911ef55fd7648630eee8815456bc993a9
Oleksandr Gneushev <galexing@gmail.com>
2024-06-03 16:42:06 +0200

    add tailwind styles

--- src/app/components/SongCard.jsx
+++ src/app/components/SongCard.jsx
@@ -1,6 +1,9 @@
 export function SongCard({ track }) {
   return (
-    <li key={track.id} className='track-card'>
+    <li
+      key={track.id}
+      className='m-5 p-2.5 bg-[rgba(203,245,92,0.3)] text-white border-2 border-[rgb(203,245,92)] rounded-[7px]'
+    >
       <p>
         Artist: {track.name} - {track.artist}
       </p>
